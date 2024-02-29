549733f24d43713d768ef2257b2db131a15cf8a0
natlopar <natalialopezariel@gmail.com>
2024-02-29 20:32:24 +0100

    CRUD

--- bd/queryMusic.sql
+++ bd/queryMusic.sql
@@ -3,9 +3,10 @@ USE FAVOURITE_MUSIC;
 CREATE TABLE album (
 idAlbum INT auto_increment primary KEY not null,
 nameAlbum varchar(50) not null,
-nameBand varchar(50) not null,
+nameBand varchar(50) not null ,
 yearAlbum INT not null, 
-`length` varchar(25) not null,
+`length` varchar(25) not null ,
 gendre varchar(25) not null
 );
+
 INSERT INTO `favourite_music`.`album` (`nameAlbum`, `nameBand`, `yearAlbum`, `length`, `gendre`) VALUES ('in utero', 'nirvana', '1993', '41:12', 'grunge');
--- src/index.js
+++ src/index.js
@@ -1,118 +1,131 @@
-//dependecias q necesito: express, cors, mysql2, dotenv
-//configurar el servidor: importo las dependencias con las q voy a trabajar:
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const api = express();
+const server = express();
 
-api.use(cors());
-api.use(express.json());
+server.use(cors());
+server.use(express.json());
 
-const port = process.env.PORT || 4500;//esto lo necesito para subir al render
+const port = process.env.PORT || 4500;
 
-async function getConnection () {
-    const connection = await mysql.createConnection({
-        host: process.env.HOST || 'localhost', 
-        user: process.env.DB_USER || 'root', 
-        password: process.env.DB_PASS || 'root',
-        database: 'recetas_db'
-    })
-    connection.connect();
-    return connection;
-};
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: process.env.HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    database: 'favourite_music',
+  });
+  connection.connect();
+  return connection;
+}
 
-api.listen(port, () => {
-    console.log(`Servidor escuchando por http://localhost:${port}`);
+server.listen(port, () => {
+  console.log(`Servidor escuchando por http://localhost:${port}`);
 });
 
 //endpoints
 
-//obtener todas las recetas. GET
-
-api.get('/recetas', async (req, res) => {
-    const connection = await getConnection();
-    const sqlRecetas = 'select * from recetas';
-    const [resultRecetas] = await connection.query(sqlRecetas);
-    const countRecetas = resultRecetas.length; //me piden el número de elementos que tengo 
-
-    res.json({
-        info: {count: countRecetas},
-        results: resultRecetas});//listado total de recetas
-})
-
-//obtener receta por ID. pruebo en navegador con localhost:4000/recetas/1
-
-api.get('/recetas/:id', async (req, res) => {
-    const idReceta = req.params.id; 
-    const connection = await getConnection();
-   
-    if(isNaN(parseInt(idReceta))) {
-        return res.json({success: false, error: 'el id debe ser un numero'})
-    }
-    const sql = 'select * from recetas where id = ?';
-    const [result] = await connection.query(sql, [idReceta]);
-    connection.end();
-    if(result.length === 0){
-        return res.json({ success: true, message: 'id no existe'})
-    }
-    res.json({success: true, receta: result[0]});
+server.post('/album', async (req, res) => {
+  const data = req.body;
+  const { nombreAlbum, nombreGrupo, año, duracion, genero } = data;
+  const connection = await getConnection();
+  const sqlInsert =
+    'insert into album (nameAlbum, nameBand, yearAlbum, length, gendre) values (?,?,?,?,?)';
+  const [result] = await connection.query(sqlInsert, [
+    nombreAlbum,
+    nombreGrupo,
+    año,
+    duracion,
+    genero,
+  ]);
+  console.log(result);
+  res.json({
+    success: true,
+    idNewAlbum: result.insertId,
+  }); //id que generó MYSQL para la nueva fila
 });
 
-api.post ('/recetas', async (req, res) => {
+server.get('/albums', async (req, res) => {
+  const connection = await getConnection();
+  const sql = 'select * from album';
+  const [result] = await connection.query(sql);
+  res.json({
+    success: true,
+    result: result,
+  });
+});
+
+// //obtener receta por ID. pruebo en navegador con localhost:4000/recetas/1
+
+// server.get('/recetas/:id', async (req, res) => {
+//     const idReceta = req.params.id;
+//     const connection = await getConnection();
+
+//     if(isNaN(parseInt(idReceta))) {
+//         return res.json({success: false, error: 'el id debe ser un numero'})
+//     }
+//     const sql = 'select * from recetas where id = ?';
+//     const [result] = await connection.query(sql, [idReceta]);
+//     connection.end();
+//     if(result.length === 0){
+//         return res.json({ success: true, message: 'id no existe'})
+//     }
+//     res.json({success: true, receta: result[0]});
+// });
+
+// //PUT = MODIFICAR UN REGISTRO
+
+server.put('/album/:id', async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const id = req.params.id;
     const data = req.body;
-    const {nombreReceta, ingredientesReceta, instruccionesReceta} = data;
-    const connection = await getConnection();
-    const sqlInsert = 'insert into recetas (nombre, ingredientes, instrucciones) values (?,?,?)';
-    const [result] = await connection.query(sql, [nombreReceta, ingredientesReceta, instruccionesReceta]);
-    console.log(result);
+    const { nombreAlbum, nombreGrupo, año, duracion, genero } = data;
+    const sql =
+      'update album set nameAlbum = ?, nameBand = ?, yearAlbum = ?, length = ?, gendre = ? where idAlbum = ?';
+    const [result] = await connection.query(sql, [
+      nombreAlbum,
+      nombreGrupo,
+      año,
+      duracion,
+      genero,
+      id,
+    ]);
     res.json({
-        success: true, 
-        id: result.insertId}) //id que generó MYSQL para la nueva fila
+      success: true,
+      message: 'álbum actualizado con éxito',
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: 'ha ocurrido un error en la modificación del álbum',
+    });
+  }
 });
 
-//PUT = MODIFICAR UN REGISTRO
-
-api.put('/recetas', async (req, res) => {
-    try{
-        const connection = await getConnection();
-        const id= req.params.id;
-        const data = req.body;
-        const {nombreReceta, ingredientesReceta, instruccionesReceta} = data;
-        const sql = 'update recetas set nombre = ?, ingredientes =?, instrucciones = ? where id = ?';
-        const [result] = await connection.query(sql, [
-            nombreReceta, ingredientesReceta, instruccionesReceta, id
-        ]);
-        res.json({
-            success: true, 
-            message: 'receta actualizada correctamente'
-        })
-    } catch (error) {
-        console.log(error)
-    }
-});
-
-
-//para el delete vamos a pasar la info por query params
-
-api.delete('/recetas', async (req, res) => {
+server.delete('/album', async (req, res) => {
+  try {
     const connection = await getConnection();
-    const idReceta = req.query.id;
-    const sql = 'delete from recetas where id = ?'
-    const [result] = await connection.query(sql, [idReceta]);
-    console.log(result);
+    const idAlbum = req.query.id;
+    const sql = 'delete from album where idAlbum = ?';
+    const [result] = await connection.query(sql, [idAlbum]);
     if (result.affectedRows > 0) {
-        res.json({
-            success: true, 
-            message: 'eliminado correctamente'
-        })
+      res.json({
+        success: true,
+        message: 'eliminado correctamente',
+      });
     } else {
-        res.json({
-            success: false, 
-            message: 'no se ha eliminado nada'
-        })
+      res.json({
+        success: false,
+        message: 'no se ha podido eliminar',
+      });
     }
-    //aquí haría un try catch
-   
-})
+  } catch (error) {
+    res.json({
+      success: false,
+      message: 'ha ocurrido un error en la petición',
+    });
+  }
+});
